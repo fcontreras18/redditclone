ebfab5fb4d3ebe4d47e34fdd7bb23d6b9956f6a2
Francisco Echeverria <fcontreras18@gmail.com>
2017-05-11 16:21:34 -0700

    Update user login

--- redditclone/urls.py
+++ redditclone/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-import accounts.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^posts/', include('posts.urls'))
 ]
