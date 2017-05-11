270b60dcd9f753ef2bf3025352ff5467bea667b2
Francisco Echeverria <fcontreras18@gmail.com>
2017-05-10 18:04:00 -0700

    Add username uniqueness

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+
 def signup(request):
-	if request.method == 'POST':
-		print("THE POST WORKED")
-	else:
-		return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['verify-password']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html',
+                    {'error': 'Username already taken'})
+
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'],
+                request.POST['password'], request.POST['verify-password'])
+                login(request, user)
+                return render(request, 'accounts/signup.html')
+        else:
+            return render(request, 'accounts/signup.html',
+                {'error': 'Passwords didn\'t match'})
+    else:
+        return render(request, 'accounts/signup.html')
